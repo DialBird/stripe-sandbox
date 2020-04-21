e48953faeb9c7cee7c74fb2edbebf6a1ba9ab2bf
keisuke <rememberthatk@icloud.com>
2020-04-22 08:23:36 +0900

    [master] add Header

--- components/Header.js
+++ components/Header.js
@@ -4,32 +4,39 @@ import Link from 'next/link'
 import AuthContext from '../contexts/Auth'
 
 const Header = () => {
-  const { currentUser, logout } = useContext(AuthContext)
+  const { currentUser, isAuthReady, logout } = useContext(AuthContext)
+
+  const renderList = () => {
+    if (!isAuthReady) return
+
+    const render = currentUser ? (
+        <li>
+          <button className='btn btn-danger' onClick={() => logout()}>ログアウト</button>
+        </li>
+      ) : (
+        <>
+        <li>
+          <Link href='/'>
+            <a className="btn btn-link">ログイン</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/signup'>
+            <a className="btn btn-link">登録</a>
+          </Link>
+        </li>
+    </>
+    )
+    return render
+  }
 
   return (
     <header className='navbar navbar-expand-lg navbar-light bg-light'>
       <div className="container">
         <h1 className="navbar-brand" href="#">Stripe Sandbox</h1>
         <ul className='navbar-nav'>
-          {currentUser ? (
-            <li>
-              <button className='btn btn-danger' onClick={() => logout()}>ログアウト</button>
-            </li>
-          ) : (
-            <>
-              <li>
-                <Link href='/'>
-                  <a className="btn btn-link">ログイン</a>
-                </Link>
-              </li>
-              <li>
-                <Link href='/signup'>
-                  <a className="btn btn-link">登録</a>
-                </Link>
-              </li>
-            </>
-          )}
-      </ul>
+          {renderList()}
+        </ul>
       </div>
     </header>
   )
