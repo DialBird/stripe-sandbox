6eb4ea33500709822318676732570711d560ddde
keisuke <rememberthatk@icloud.com>
2020-04-22 17:40:58 +0900

    [master] call session and setup init

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,28 @@
-const Dashboard = () => {
+import { parseCookies } from 'nookies'
+
+import StripeForm from '../components/StripeForm'
+
+const Dashboard = ({ userId }) => {
   return (
     <div className='dashboard-layout'>
       <h2 className='dashboard-layout__page-title'>ダッシュボード</h2>
+      <div className='shadow payment-box'>
+        <StripeForm userId={userId} />
+      </div>
     </div>
   )
+}
+
+export const getServerSideProps = async ctx => {
+  const { sesssion } = parseCookies(ctx)
+  const { userId, userName } = JSON.parse(sesssion)
+
+  return {
+    props: {
+      userId,
+      userName
+    }
+  }
 }
 
 export default Dashboard
