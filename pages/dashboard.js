9b576ee1956269d1344f75f2a9511474cbcc77cb
keisuke <rememberthatk@icloud.com>
2020-04-23 11:02:36 +0900

    [master] add unlink link

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -10,6 +10,7 @@ const Dashboard = () => {
   const {
     fetchingStripeAccountId,
     stripeAccountId,
+    unlinkStripe,
     userId
   } = useContext(StripeContext)
   const stripePromise = loadStripe(process.env.STRIPE_PUBLIC)
@@ -23,6 +24,7 @@ const Dashboard = () => {
           <Elements stripe={stripePromise}>
             <CheckoutForm/>
           </Elements>
+          <button onClick={unlinkStripe} className='btn btn-link'>連携解除</button>
         </div>
       )
     } else {
