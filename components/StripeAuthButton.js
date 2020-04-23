dcf4cb469e719de950411b5c91d365643d1eafeb
keisuke <rememberthatk@icloud.com>
2020-04-23 13:36:39 +0900

    [master] add client id to env

--- components/StripeAuthButton.js
+++ components/StripeAuthButton.js
@@ -2,7 +2,7 @@ const BASE_URL = 'https://connect.stripe.com/oauth/authorize'
 
 const StripeAuthButton = ({userId}) => {
   let url = BASE_URL
-  const clientId = 'ca_H8bpyHXORpTHFxQprZet4RoayZpGguXY'
+  const clientId = process.env.STRIPE_CLIENT_ID
   const redirectTo = 'http://localhost:3000/api/auth/stripe/callback'
   url += `?response_type=code`
   url += `&client_id=${clientId}`
